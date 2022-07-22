498aa7ae1721aa2dab08ea201509d738a25c57f7
Sharath-Win64 <sharathwelcomesyouin@gmail.com>
2022-07-22 14:51:22 +0530

    not working in ap-south-1

--- securitygroup.tf
+++ securitygroup.tf
@@ -4,13 +4,24 @@ data "aws_ip_ranges" "mumbai_ec2" {
 }
 
 resource "aws_security_group" "from_mumbai" {
+    name="from_mumbai"
   ingress = {
     from_port="443"
     to_port="443"
     protocol="tcp"
-    cidr_blocks=["${data.aws_ip_ranges.mumbai_ec2.cidr_blocks}"] #mumbai_ec2.cidr_blocks is a attribute
+    cidr_blocks= slice(data.aws_ip_ranges.mumbai_ec2.cidr_blocks, 0, 50) #mumbai_ec2.cidr_blocks is a attribute
 
   }
+
+#    ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = [aws_vpc.main.cidr_block]
+#     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+#   }
+
 
   tags = {
     CreateDate="${data.aws_ip_ranges.mumbai_ec2.create_date}"
